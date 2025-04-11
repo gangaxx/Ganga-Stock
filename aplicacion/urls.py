c7f2c337ae4dd31517a2c381f502ca405ab771fe
MrDrenix <to.sotomayor@duocuc.cl>
2025-04-11 18:09:52 -0400

    Base funcional

---
+++ aplicacion/urls.py
@@ -0,0 +1,13 @@
+# aplicacion/urls.py
+from django.urls import path
+from aplicacion import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
